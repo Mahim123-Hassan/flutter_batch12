060ffa5f35f44ed8129f07e979b2679516305909
Mahim123-Hassan <hmahim951@gmail.com>
2026-07-08 22:48:36 +0600

    _

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_batch_12/home_screen.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(
+
+      ),
+    );
   }
 }
