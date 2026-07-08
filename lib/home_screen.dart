5a90209ff1a450af32045e6f03fd2d10b510ef93
Mahim123-Hassan <hmahim951@gmail.com>
2026-07-08 23:15:14 +0600

    _

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,14 +9,27 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Mahim"),
         centerTitle: true,
-
-
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back)),
+        actions: [
+          Icon(Icons.search),
+          SizedBox(width: 10),
+          Icon(Icons.notifications),
+        ],
+        backgroundColor: Colors.lightBlue,
+        foregroundColor: Colors.white,
+        elevation: 0,
+        toolbarHeight: 50,
+        shadowColor: Colors.red,
+        surfaceTintColor: Colors.transparent,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.blue, Colors.pinkAccent]),
+          ),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(12)),
+        ),
       ),
-        
-      
-      
-
-
     );
   }
 }
